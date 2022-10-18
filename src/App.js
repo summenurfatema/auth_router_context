ef303f0d7e866437a88189dd89760cadb2c2273e
Fatema04 <shadrinmoni15@gmail.com>
2022-10-18 05:32:40 -0700

    route added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Main from './layout/Main';
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/', element:
+      path: '/', element: <Main></Main>, children: [
+        {
+          path: '/', element: <Home></Home>
+        },
+        {
+          path: '/login', element: <Login></Login>
+        },
+        {
+          path: '/register', element: <Register></Register>
+        }
+      ]
     }
   ])
   return (
     <div className="App">
 
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
