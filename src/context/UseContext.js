c22ac1d30d8f87d39d3af221faf75a7394f9352a
Fatema04 <shadrinmoni15@gmail.com>
2022-10-18 20:01:28 -0700

    Context

--- src/context/UseContext.js
+++ src/context/UseContext.js
@@ -1,9 +1,17 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import app from '../firebae/firebase.config'
 
+
+const auth = getAuth(app)
 export const AuthContext = createContext()
 const UseContext = ({ children }) => {
-    const user = { displayName: 'akash' };
-    const authInfo = { user: user }
+    const [user, setUser] = useState({ displayName: 'akash' })
+
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const authInfo = { user, createUser }
     return (
         <div>
             <AuthContext.Provider value={authInfo}>
